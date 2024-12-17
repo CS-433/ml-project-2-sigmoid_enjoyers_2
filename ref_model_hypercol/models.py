a1c623a16f8f151a57aa45674bec7d31ca8f4106
John Hausberg Anfindsen <john.h.anfindsen@gmail.com>
2024-12-17 17:51:10 +0100

    push model implementation

---
+++ ref_model_hypercol/models.py
@@ -0,0 +1,386 @@
+from torch import nn
+import torch
+import torchvision
+from torch.nn import functional as F
+import pretrainedmodels
+
+from . import blocks
+
+
+class UNetResNextHyperSE(nn.Module):
+    """PyTorch U-Net model using ResNet(34, 101 or 152) encoder.
+
+    UNet: https://arxiv.org/abs/1505.04597
+    ResNext: https://arxiv.org/abs/1611.05431
+
+    Args:
+    encoder_depth (int): Depth of a ResNet encoder (34, 101 or 152).
+    num_classes (int): Number of output classes.
+    num_filters (int, optional): Number of filters in the last layer of decoder. Defaults to 32.
+    dropout_2d (float, optional): Probability factor of dropout layer before output layer. Defaults to 0.2.
+    is_deconv (bool, optional):
+        False: bilinear interpolation is used in decoder.
+        True: deconvolution is used in decoder.
+        Defaults to False.
+
+    """
+
+    def __init__(self, encoder_depth, num_classes, num_filters=32, dropout_2d=0.2, is_deconv=False):
+        super().__init__()
+        self.num_classes = num_classes
+        self.dropout_2d = dropout_2d
+        
+        # changed version of the original code
+        if encoder_depth == 50:
+            self.encoder = pretrainedmodels.__dict__['se_resnext50_32x4d'](num_classes=1000, pretrained=None)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 101:
+            self.encoder = pretrainedmodels.__dict__['se_resnext101_32x4d'](num_classes=1000, pretrained=None)
+            bottom_channel_nr = 2048
+        else:
+            raise NotImplementedError('only 50, 101 versions of SE-ResNeXt are implemented')
+
+        # Modify the first convolution layer to accept 7 channels
+        old_conv1 = self.encoder.layer0.conv1
+        self.encoder.layer0.conv1 = nn.Conv2d(
+            in_channels=7,  # Update to match your input channels
+            out_channels=old_conv1.out_channels,
+            kernel_size=old_conv1.kernel_size,
+            stride=old_conv1.stride,
+            padding=old_conv1.padding,
+            bias=old_conv1.bias
+        )
+
+        # Initialize weights for the new convolution layer
+        with torch.no_grad():
+            self.encoder.layer0.conv1.weight[:, :3, :, :] = old_conv1.weight  # Copy RGB weights
+            self.encoder.layer0.conv1.weight[:, 3:, :, :] = old_conv1.weight.mean(dim=1, keepdim=True)  # Average for extra channels
+
+
+        '''if encoder_depth == 50:
+            self.encoder = pretrainedmodels.__dict__['se_resnext50_32x4d'](num_classes=1000, pretrained=None)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 101:
+            self.encoder = pretrainedmodels.__dict__['se_resnext101_32x4d'](num_classes=1000, pretrained=None)
+            bottom_channel_nr = 2048
+        else:
+            raise NotImplementedError('only 34, 101, 152 version of Resnet are implemented')'''
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.input_adjust = blocks.EncoderBlock(
+            nn.Sequential(self.encoder.layer0.conv1, self.encoder.layer0.bn1, self.encoder.layer0.relu1, self.pool),
+            num_filters * 2,
+        )
+
+        self.conv1 = blocks.EncoderBlock(self.encoder.layer1, bottom_channel_nr // 8)
+        self.conv2 = blocks.EncoderBlock(self.encoder.layer2, bottom_channel_nr // 4)
+        self.conv3 = blocks.EncoderBlock(self.encoder.layer3, bottom_channel_nr // 2)
+        self.conv4 = blocks.EncoderBlock(self.encoder.layer4, bottom_channel_nr)
+
+        self.dec4 = blocks.DecoderBlockV4(bottom_channel_nr, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec3 = blocks.DecoderBlockV4(bottom_channel_nr // 2 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec2 = blocks.DecoderBlockV4(bottom_channel_nr // 4 + num_filters * 8, num_filters * 4 * 2, num_filters * 2, is_deconv)
+        self.dec1 = blocks.DecoderBlockV4(bottom_channel_nr // 8 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2, is_deconv)
+
+        # Adjusted to handle 28-channel input
+        self.final = nn.Conv2d(28, self.num_classes, kernel_size=1)
+
+        # Deep supervision
+        self.dsv4 = blocks.UnetDsv3(in_size=num_filters * 8, out_size=num_classes, scale_factor=8 * 2)
+        self.dsv3 = blocks.UnetDsv3(in_size=num_filters * 8, out_size=num_classes, scale_factor=4 * 2)
+        self.dsv2 = blocks.UnetDsv3(in_size=num_filters * 2, out_size=num_classes, scale_factor=2 * 2)
+        self.dsv1 = nn.Conv2d(in_channels=num_filters * 2 * 2, out_channels=num_classes, kernel_size=1)
+
+
+    '''def __init__(self, encoder_depth, num_classes, num_filters=32, dropout_2d=0.2, is_deconv=False):
+        super().__init__()
+        self.num_classes = num_classes
+        self.dropout_2d = dropout_2d
+
+        if encoder_depth == 50:
+            # modified from original code; before: pretrained='imagenet', after: pretrained=None
+            self.encoder = pretrainedmodels.__dict__['se_resnext50_32x4d'](num_classes=1000, pretrained=None) 
+            bottom_channel_nr = 2048
+        elif encoder_depth == 101:
+            # modified from original code; before: pretrained='imagenet', after: pretrained=None
+            self.encoder = pretrainedmodels.__dict__['se_resnext101_32x4d'](num_classes=1000, pretrained=None) 
+            bottom_channel_nr = 2048
+        else:
+            raise NotImplementedError('only 34, 101, 152 version of Resnet are implemented')
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+        
+        # conv1 is modified in order to handle 7 channel input
+        #--------------original version----------------
+        #conv1 = nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(1, 1), padding=(3, 3), bias=False)
+        #conv1.weight = self.encoder.layer0.conv1.weight
+        #------------------------------------------------
+        #--------------modified version----------------
+        conv1 = nn.Conv2d(7, 64, kernel_size=(7, 7), stride=(1, 1), padding=(3, 3), bias=False)
+        # Initialize weights for the 7-channel input by averaging the pre-trained weights for the 3-channel input
+        if hasattr(self.encoder.layer0, "conv1"):
+            pretrained_weights = self.encoder.layer0.conv1.weight
+            conv1.weight.data[:, :3, :, :] = pretrained_weights  # Copy weights for RGB channels
+            conv1.weight.data[:, 3:, :, :] = pretrained_weights.mean(dim=1, keepdim=True)  # Average weights for additional channels
+        #------------------------------------------------
+         
+
+        self.input_adjust = blocks.EncoderBlock(
+            nn.Sequential(conv1, self.encoder.layer0.bn1, self.encoder.layer0.relu1, self.pool),
+            num_filters*2
+        )
+
+        self.conv1 = blocks.EncoderBlock(self.encoder.layer1, bottom_channel_nr//8)
+        self.conv2 = blocks.EncoderBlock(self.encoder.layer2, bottom_channel_nr//4)
+        self.conv3 = blocks.EncoderBlock(self.encoder.layer3, bottom_channel_nr//2)
+        self.conv4 = blocks.EncoderBlock(self.encoder.layer4, bottom_channel_nr)
+
+        self.dec4 = blocks.DecoderBlockV4(bottom_channel_nr, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec3 = blocks.DecoderBlockV4(bottom_channel_nr // 2 + num_filters * 8, num_filters * 8 * 2, num_filters * 8,
+                                   is_deconv)
+        self.dec2 = blocks.DecoderBlockV4(bottom_channel_nr // 4 + num_filters * 8, num_filters * 4 * 2, num_filters * 2,
+                                   is_deconv)
+        self.dec1 = blocks.DecoderBlockV4(bottom_channel_nr // 8 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2,
+                                   is_deconv)
+        self.final = nn.Conv2d(2 * 2, num_classes, kernel_size=1)
+
+        # deep supervision
+        self.dsv4 = blocks.UnetDsv3(in_size=num_filters * 8, out_size=num_classes, scale_factor=8 * 2)
+        self.dsv3 = blocks.UnetDsv3(in_size=num_filters * 8, out_size=num_classes, scale_factor=4 * 2)
+        self.dsv2 = blocks.UnetDsv3(in_size=num_filters * 2, out_size=num_classes, scale_factor=2 * 2)
+        self.dsv1 = nn.Conv2d(in_channels=num_filters * 2 * 2, out_channels=num_classes, kernel_size=1)'''
+    
+    # modified forward function
+    def forward(self, x):
+        input_adjust = self.input_adjust(x)
+        conv1 = self.conv1(input_adjust)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        center = self.conv4(conv3)
+        dec4 = self.dec4(center)
+        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
+        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
+        dec1 = self.dec1(torch.cat([dec2, conv1], 1))
+
+        # Deep Supervision
+        dsv4 = self.dsv4(dec4)
+        dsv3 = self.dsv3(dec3)
+        dsv2 = self.dsv2(dec2)
+        dsv1 = self.dsv1(dec1)
+
+        # Concatenate deep supervision outputs
+        dsv0 = torch.cat([dsv1, dsv2, dsv3, dsv4], dim=1)
+
+        # Reduce concatenated output to the number of classes
+        final_output = self.final(dsv0)
+
+        # Upsample to match the size of the target labels
+        final_output = F.interpolate(final_output, size=(128, 128), mode="bilinear", align_corners=False)
+
+        return final_output
+
+
+
+    '''def forward(self, x):
+        input_adjust = self.input_adjust(x)
+        conv1 = self.conv1(input_adjust)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+
+        center = self.conv4(conv3)
+
+        dec4 = self.dec4(center)
+        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
+        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
+        dec1 = self.dec1(torch.cat([dec2, conv1], 1))
+
+        # Deep Supervision
+        dsv4 = self.dsv4(dec4)
+        dsv3 = self.dsv3(dec3)
+        dsv2 = self.dsv2(dec2)
+        dsv1 = self.dsv1(dec1)
+        dsv0 = torch.cat([dsv1, dsv2, dsv3, dsv4], dim=1)
+
+        return self.final(dsv0)'''
+
+
+class UNetResNext(nn.Module):
+    """PyTorch U-Net model using ResNet(34, 101 or 152) encoder.
+
+    UNet: https://arxiv.org/abs/1505.04597
+    ResNext: https://arxiv.org/abs/1611.05431
+
+    Args:
+    encoder_depth (int): Depth of a ResNet encoder (34, 101 or 152).
+    num_classes (int): Number of output classes.
+    num_filters (int, optional): Number of filters in the last layer of decoder. Defaults to 32.
+    dropout_2d (float, optional): Probability factor of dropout layer before output layer. Defaults to 0.2.
+    is_deconv (bool, optional):
+        False: bilinear interpolation is used in decoder.
+        True: deconvolution is used in decoder.
+        Defaults to False.
+
+    """
+
+    def __init__(self, encoder_depth, num_classes, num_filters=32, dropout_2d=0.2, is_deconv=False):
+        super().__init__()
+        self.num_classes = num_classes
+        self.dropout_2d = dropout_2d
+
+        if encoder_depth == 50:
+            self.encoder = pretrainedmodels.__dict__['se_resnext50_32x4d'](num_classes=1000, pretrained='imagenet')
+            bottom_channel_nr = 2048
+        elif encoder_depth == 101:
+            self.encoder = pretrainedmodels.__dict__['se_resnext101_32x4d'](num_classes=1000, pretrained='imagenet')
+            bottom_channel_nr = 2048
+        else:
+            raise NotImplementedError('only 34, 101, 152 version of Resnet are implemented')
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.input_adjust = nn.Sequential(self.encoder.layer0.conv1,
+                                          self.encoder.layer0.bn1,
+                                          self.encoder.layer0.relu1)
+
+        self.conv1 = self.encoder.layer1
+        self.conv2 = self.encoder.layer2
+        self.conv3 = self.encoder.layer3
+        self.conv4 = self.encoder.layer4
+
+        self.dec4 = blocks.DecoderBlockV2(bottom_channel_nr, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec3 = blocks.DecoderBlockV2(bottom_channel_nr // 2 + num_filters * 8, num_filters * 8 * 2, num_filters * 8,
+                                   is_deconv)
+        self.dec2 = blocks.DecoderBlockV2(bottom_channel_nr // 4 + num_filters * 8, num_filters * 4 * 2, num_filters * 2,
+                                   is_deconv)
+        self.dec1 = blocks.DecoderBlockV2(bottom_channel_nr // 8 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2,
+                                   is_deconv)
+        self.final = nn.Conv2d(num_filters * 2 * 2, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        input_adjust = self.input_adjust(x)
+        conv1 = self.conv1(input_adjust)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        center = self.conv4(conv3)
+        dec4 = self.dec4(center)
+        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
+        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
+        dec1 = F.dropout2d(self.dec1(torch.cat([dec2, conv1], 1)), p=self.dropout_2d)
+
+        return self.final(dec1)
+
+
+class UNetResNet(nn.Module):
+    """PyTorch U-Net model using ResNet(34, 101 or 152) encoder.
+
+    UNet: https://arxiv.org/abs/1505.04597
+    ResNet: https://arxiv.org/abs/1512.03385
+
+    Args:
+    encoder_depth (int): Depth of a ResNet encoder (34, 101 or 152).
+    num_classes (int): Number of output classes.
+    num_filters (int, optional): Number of filters in the last layer of decoder. Defaults to 32.
+    dropout_2d (float, optional): Probability factor of dropout layer before output layer. Defaults to 0.2.
+    pretrained (bool, optional):
+        False - no pre-trained weights are being used.
+        True  - ResNet encoder is pre-trained on ImageNet.
+        Defaults to False.
+    is_deconv (bool, optional):
+        False: bilinear interpolation is used in decoder.
+        True: deconvolution is used in decoder.
+        Defaults to False.
+
+    """
+
+    def __init__(self, encoder_depth, num_classes, num_filters=32, dropout_2d=0.2,
+                 pretrained=False, is_deconv=False):
+
+        super().__init__()
+        self.num_classes = num_classes
+        self.dropout_2d = dropout_2d
+
+        if encoder_depth == 34:
+            self.encoder = torchvision.models.resnet34(pretrained=pretrained)
+            bottom_channel_nr = 512
+        elif encoder_depth == 101:
+            self.encoder = torchvision.models.resnet101(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 152:
+            self.encoder = torchvision.models.resnet152(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        else:
+            raise NotImplementedError('only 34, 101, 152 version of Resnet are implemented')
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.input_adjust = nn.Sequential(self.encoder.conv1,
+                                          self.encoder.bn1,
+                                          self.encoder.relu)
+
+        self.conv1 = self.encoder.layer1
+        self.conv2 = self.encoder.layer2
+        self.conv3 = self.encoder.layer3
+        self.conv4 = self.encoder.layer4
+
+        self.dec4 = blocks.DecoderBlockV2(bottom_channel_nr, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec3 = blocks.DecoderBlockV2(bottom_channel_nr // 2 + num_filters * 8, num_filters * 8 * 2, num_filters * 8,
+                                   is_deconv)
+        self.dec2 = blocks.DecoderBlockV2(bottom_channel_nr // 4 + num_filters * 8, num_filters * 4 * 2, num_filters * 2,
+                                   is_deconv)
+        self.dec1 = blocks.DecoderBlockV2(bottom_channel_nr // 8 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2,
+                                   is_deconv)
+        self.final = nn.Conv2d(num_filters * 2 * 2, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        input_adjust = self.input_adjust(x)
+        conv1 = self.conv1(input_adjust)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        center = self.conv4(conv3)
+        dec4 = self.dec4(center)
+        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
+        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
+        dec1 = F.dropout2d(self.dec1(torch.cat([dec2, conv1], 1)), p=self.dropout_2d)
+
+        return self.final(dec1)
+
+
+def get_model(model_type, **model_pars):
+    """
+    Create a new model
+
+    :param model_type: str, type of model, one of ['UNet11', 'UNet16', 'LinkNet34', 'UNet']
+    :param model_pars: dict, initialization parameters for the model
+    :return: instance of nn.Module, a new Pytorch model
+    """
+
+    if model_type == 'UNetResNet34':
+        return UNetResNet(encoder_depth=34, num_classes=1, pretrained=True,  num_filters=32, is_deconv=True, dropout_2d=0.2)
+
+    if model_type == 'UNetResNet101':
+        return UNetResNet(encoder_depth=101, num_classes=1, pretrained=True, num_filters=32, is_deconv=True, dropout_2d=0.2)
+
+    if model_type == 'UNetResNet152':
+        return UNetResNet(encoder_depth=152, num_classes=1, pretrained=True, num_filters=32, is_deconv=True, dropout_2d=0.2)
+
+    if model_type == 'UNetResNext50':
+        return UNetResNext(encoder_depth=50, num_classes=1, num_filters=16, is_deconv=True, dropout_2d=0)
+
+    if model_type == 'UNetResNext101_32x4d':
+        return UNetResNext(encoder_depth=101, num_classes=1, num_filters=32, is_deconv=True, dropout_2d=0.2)
+
+    if model_type == 'UNetResNextHyperSE50':
+        return UNetResNextHyperSE(encoder_depth=50, num_classes=1, num_filters=32, is_deconv=True, dropout_2d=0.2)
+
+    if model_type == 'UNetResNextHyperSE101':
+        return UNetResNextHyperSE(encoder_depth=101, num_classes=1, num_filters=32, is_deconv=True, dropout_2d=0.2)
+
+    raise ValueError('Unknown model type: {}'.format(model_type))
